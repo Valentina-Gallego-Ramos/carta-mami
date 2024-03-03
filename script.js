6d61cd193aeabfc296f5baba10455327bd3132f5
Valentina-Gallego-Ramos <144750192+Valentina-Gallego-Ramos@users.noreply.github.com>
2024-03-03 16:37:19 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+ <script>
+    var play = document.getElementById("play");
+    var song = document.getElementById("song");
+    play.addEventListener("click", function() {
+      song.play();
+    });
+  </script>
+
